81f6ed72b65401cb00bac264ae65fb3094db2e59
alexanderajju <alexanderajju@gmail.com>
2020-09-09 09:35:56 +0530

    Header completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Amazon</h1>
-      <Header/>
+      <Header />
       {/* Home */}
     </div>
   );
