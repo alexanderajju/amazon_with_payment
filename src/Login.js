331e118eff5ef5c1c230edb73144f8799a575aea
alexanderajju <alexanderajju@gmail.com>
2020-09-10 09:13:06 +0530

    SignIn  page

---
+++ src/Login.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+  const signUp = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign in</h1>
+        <form action="">
+          <h5>Email</h5>
+          <input
+            value={email}
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            value={password}
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            onClick={signIn}
+            type="submit"
+            className="login__signInButton"
+          >
+            Sign in
+          </button>
+        </form>
+        <p>
+          By sigining-in you agree to Amazon's Clone Conditons of Use & Sale ,
+          Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads
+        </p>
+        <button className="login__registerButton" onClick={signUp}>
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBMfHScLtJKoDxMUjTLnakfeqJ5Dencq_I",
+    authDomain: "full-7632a.firebaseapp.com",
+    databaseURL: "https://full-7632a.firebaseio.com",
+    projectId: "full-7632a",
+    storageBucket: "full-7632a.appspot.com",
+    messagingSenderId: "373410294541",
+    appId: "1:373410294541:web:919c2ec47d4cadb7d19e44"
+  };
