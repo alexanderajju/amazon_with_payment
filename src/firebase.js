f8150d6980efa83701eb4788bd01003be1853df3
alexanderajju <alexanderajju@gmail.com>
2020-09-10 10:09:32 +0530

    User creation completed

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,19 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBMfHScLtJKoDxMUjTLnakfeqJ5Dencq_I",
-    authDomain: "full-7632a.firebaseapp.com",
-    databaseURL: "https://full-7632a.firebaseio.com",
-    projectId: "full-7632a",
-    storageBucket: "full-7632a.appspot.com",
-    messagingSenderId: "373410294541",
-    appId: "1:373410294541:web:919c2ec47d4cadb7d19e44"
-  };
+  apiKey: "AIzaSyBMfHScLtJKoDxMUjTLnakfeqJ5Dencq_I",
+  authDomain: "full-7632a.firebaseapp.com",
+  databaseURL: "https://full-7632a.firebaseio.com",
+  projectId: "full-7632a",
+  storageBucket: "full-7632a.appspot.com",
+  messagingSenderId: "373410294541",
+  appId: "1:373410294541:web:919c2ec47d4cadb7d19e44",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export { db, auth };
