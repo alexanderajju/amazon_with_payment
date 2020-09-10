5f7bbee69a679f44e16f4b75ed004ad077ed5ffe
alexanderajju <alexanderajju@gmail.com>
2020-09-10 07:54:42 +0530

    remove basket using index

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./CheckoutProduct.css";
+import {useStateValue} from './StateProvider'
 
 function CheckoutProduct({ id, img, title, price, rating }) {
-  const removeFromBasket = () => {
-      
+  
+  const [{basket}, dispatch]=useStateValue();
+    const removeFromBasket = () => {
+        dispatch(
+      { type: 'REMOVE_FROM_BASKET',
+    id:id, }
+    )
+
   };
   return (
     <div className="checkoutProduct">
