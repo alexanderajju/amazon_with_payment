f2c10821ede91bb215b93b0262de1746f274650b
alexanderajju <alexanderajju@gmail.com>
2020-09-11 10:49:43 +0530

    Stripe payment

--- src/Payment.js
+++ src/Payment.js
@@ -1,12 +1,60 @@
-import React from 'react'
-import "./Payment.css"
+import React from "react";
+import "./Payment.css";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+
+import { useHistory, Link } from "react-router-dom";
 
 function Payment() {
-    return (
-        <div className="payment">
-            <h1>Payment</h1>
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        <h1>
+          Checkout(<Link to="/checkout"> {basket?.length} order</Link>)
+        </h1>
+        {/* Payment Section */}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 React Lane</p>
+            <p>Los Angles, CA</p>
+          </div>
         </div>
-    )
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                key={item.id}
+                title={item.title}
+                id={item.id}
+                img={item.img}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+        {/* Paymet section - Review Item */}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details"></div>
+        </div>
+
+        {/* Payment Method */}
+      </div>
+    </div>
+  );
 }
 
-export default Payment
+export default Payment;
