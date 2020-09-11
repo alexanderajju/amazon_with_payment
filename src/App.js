b229d39c1469d56674e7eb987a410eb005c1f1bf
alexanderajju <alexanderajju@gmail.com>
2020-09-11 08:00:07 +0530

    Payment page create

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Checkout from "./Checkout";
 import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import Payment from "./Payment";
 
 function App() {
   const [{ user }, dispatch] = useStateValue();
@@ -23,6 +24,10 @@ function App() {
     <Router>
       <div className="App">
         <Switch>
+          <Route path="/payment">
+            <Header />
+            <Payment/>
+          </Route>
           <Route path="/login">
             <Login />
           </Route>
--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import Product from "./Product";
 function Home() {
   return (
     <div className="home">
-      <div className="home__container">
+      <div className="home__container ">
         <img
           className="home__image"
           //   src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/Xiaomi/Redmi_8A_Dual/7499/V176845577_IN_WLME_Redmi_8A_Dual_LandingPage_1500x600._CB406191253_.jpg"
